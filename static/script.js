f2842aedabacfb6dfa49f24e3263027636199186
rashi <rashighosh96@gmail.com>
2024-03-24 21:24:24 -0400

    edits

--- static/script.js
+++ static/script.js
@@ -1,8 +1,61 @@
 const video = document.getElementById('myVideo');
+const chatBox = document.getElementById('chat-box');
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function checkModal(message, audioDataUrl) {
+    // Check the variable every 1 second (1000 milliseconds)
+var intervalId = setInterval(function() {
+    // Check if the variable is true
+    if (document.getElementById("myModal").style.display == "none") {
+        // Execute your function
+        appendAlexMessage2(message, audioDataUrl);
+        clearInterval(intervalId);
+    }
+}, 1000); // Interval set to 1000 milliseconds (1 second)
+}
+
+function appendAlexMessage2(message, audioDataUrl) {
+    // Append the text message
+    const messageElement = document.createElement('div');
+    const labelText = document.createElement('span');
+    labelText.className = "label-text";
+    const messageText = document.createElement('span');
+
+    labelText.innerText = `Alex: `;
+    messageText.innerText = `${message}`;
+
+    messageElement.className = "chatbot-message"
+    messageElement.appendChild(labelText);
+    messageElement.appendChild(messageText);
+    chatBox.appendChild(messageElement);
+
+    // Create and append the audio element
+    const audioElement = new Audio(audioDataUrl);
+    audioElement.controls = true;
+    chatBox.appendChild(audioElement);
+    audioElement.style.display = 'none'
+
+    // Play the video and loop when the audio starts playing
+    audioElement.addEventListener('play', function() {
+        video.loop = true; // Ensure video loops
+        video.play();
+    });
+
+    // Pause the video when the audio stops playing
+    audioElement.addEventListener('ended', function() {
+        video.currentTime = video.duration;
+        video.pause();
+    });
+
+    audioElement.play();
+
+    chatBox.scrollTop = chatBox.scrollHeight; // Scroll to bottom
+}
 
 function appendAlexMessage(message, audioDataUrl) {
-    const chatBox = document.getElementById('chat-box');
-    
     // Append the text message
     const messageElement = document.createElement('div');
     const labelText = document.createElement('span');
@@ -117,7 +170,7 @@ function sendMessage() {
     })
     .then(response => response.json())
     .then(data => {
-        appendAlexMessage(data.message, data.audio);
+        appendAlexMessage2(data.message, data.audio);
     })
     .catch(error => console.error('Error:', error))
     .finally(() => {
@@ -128,3 +181,40 @@ function sendMessage() {
 
     userInput.value = ''; // Clear input field after sending message
 }
+
+window.onload = function() {
+    console.log("IN ON LOAD")
+    let userMessage = "Introduce yourself as Alex, an AI virtual healthcare assistant. What are the kinds of questions you can answer? Keep your response short, less than 75 tokens."
+    // Actions to be performed when the page is fully loaded
+    const ellipse = document.createElement('div');
+    ellipse.className = "lds-ellipsis";
+    ellipse.setAttribute('id', "lds-ellipsis")
+
+    const l1 = document.createElement('div');
+    const l2 = document.createElement('div');
+    const l3 = document.createElement('div');
+
+    ellipse.appendChild(l1)
+    ellipse.appendChild(l2)
+    ellipse.appendChild(l3)
+
+    // const chatBox = document.getElementById('chat-box');
+    chatBox.appendChild(ellipse);
+
+    fetch('/api/chatbot', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({message: userMessage})
+    })
+    .then(response => response.json())
+    .then(data => {
+        checkModal(data.message, data.audio);
+    })
+    .catch(error => console.error('Error:', error))
+    .finally(() => {
+        // Remove loading indicator after response received
+        const ellipse = document.getElementById('lds-ellipsis');
+        ellipse.remove();
+    });
+  };
+  
